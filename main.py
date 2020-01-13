1669519e5cd0081f64e701670f554951dab95459
irfanhilmi04 <56240221+irfanhilmi04@users.noreply.github.com>
2020-01-13 09:54:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,20 @@ if __name__ == "__main__":
     app = MainApp()
     app.run()
 
+class list_book():
+    view = ViewBook(self.books)
+    view.list()
+
+
+def add_book(self):
+    add = InputBook()
+    add.input()
+
+
+def search_book():
+    cari = InputBook()
+    cari.search()
+    help = SearchHelper()
+    help.search_title()
+    liat = ViewBook(books=self)
+    liat.list()
